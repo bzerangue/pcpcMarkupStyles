e911c0e42d430dc236e88b5a763c53784ef89455
Lindsey <lindseyb@wearetelegraph.com>
2012-12-11 17:51:56 -0600

    More Layouts

--- js/foundation/app.js
+++ js/foundation/app.js
@@ -48,10 +48,21 @@ $(window).load(function() {
  });
 
 
+/**
+* hoverIntent r6 // 2011.02.26 // jQuery 1.5.1+
+* <http://cherne.net/brian/resources/jquery.hoverIntent.html>
+* 
+* @param  f  onMouseOver function || An object with configuration options
+* @param  g  onMouseOut function  || Nothing (use configuration options object)
+* @author    Brian Cherne brian(at)cherne(dot)net
+*/
+(function($){$.fn.hoverIntent=function(f,g){var cfg={sensitivity:7,interval:100,timeout:0};cfg=$.extend(cfg,g?{over:f,out:g}:f);var cX,cY,pX,pY;var track=function(ev){cX=ev.pageX;cY=ev.pageY};var compare=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);if((Math.abs(pX-cX)+Math.abs(pY-cY))<cfg.sensitivity){$(ob).unbind("mousemove",track);ob.hoverIntent_s=1;return cfg.over.apply(ob,[ev])}else{pX=cX;pY=cY;ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}};var delay=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);ob.hoverIntent_s=0;return cfg.out.apply(ob,[ev])};var handleHover=function(e){var ev=jQuery.extend({},e);var ob=this;if(ob.hoverIntent_t){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t)}if(e.type=="mouseenter"){pX=ev.pageX;pY=ev.pageY;$(ob).bind("mousemove",track);if(ob.hoverIntent_s!=1){ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}}else{$(ob).unbind("mousemove",track);if(ob.hoverIntent_s==1){ob.hoverIntent_t=setTimeout(function(){delay(ev,ob)},cfg.timeout)}}};return this.bind('mouseenter',handleHover).bind('mouseleave',handleHover)}})(jQuery);
+
+
+
 
 // Mega Drop JS ---
 (function(a){a(function(){a(".toggleMegaDrop").live("click",function(b){var c=a(b.currentTarget);c.hasClass("open")?(c.removeClass("open"),a("#megaDrop").slideUp(400)):(c.addClass("open"),a("#megaDrop").slideDown(400))})})})(jQuery);
-
 
 
  $(document).ready(function() {
@@ -79,12 +90,53 @@ $(window).load(function() {
     });
 
 
-/**
-* hoverIntent r6 // 2011.02.26 // jQuery 1.5.1+
-* <http://cherne.net/brian/resources/jquery.hoverIntent.html>
-* 
-* @param  f  onMouseOver function || An object with configuration options
-* @param  g  onMouseOut function  || Nothing (use configuration options object)
-* @author    Brian Cherne brian(at)cherne(dot)net
-*/
-(function($){$.fn.hoverIntent=function(f,g){var cfg={sensitivity:7,interval:100,timeout:0};cfg=$.extend(cfg,g?{over:f,out:g}:f);var cX,cY,pX,pY;var track=function(ev){cX=ev.pageX;cY=ev.pageY};var compare=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);if((Math.abs(pX-cX)+Math.abs(pY-cY))<cfg.sensitivity){$(ob).unbind("mousemove",track);ob.hoverIntent_s=1;return cfg.over.apply(ob,[ev])}else{pX=cX;pY=cY;ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}};var delay=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);ob.hoverIntent_s=0;return cfg.out.apply(ob,[ev])};var handleHover=function(e){var ev=jQuery.extend({},e);var ob=this;if(ob.hoverIntent_t){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t)}if(e.type=="mouseenter"){pX=ev.pageX;pY=ev.pageY;$(ob).bind("mousemove",track);if(ob.hoverIntent_s!=1){ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}}else{$(ob).unbind("mousemove",track);if(ob.hoverIntent_s==1){ob.hoverIntent_t=setTimeout(function(){delay(ev,ob)},cfg.timeout)}}};return this.bind('mouseenter',handleHover).bind('mouseleave',handleHover)}})(jQuery);
+
+
+
+// Sticky Footer Plugin
+(function($){
+  var footer;
+
+  $.fn.extend({
+    stickyFooter: function(options) {
+      footer = this;
+      
+      positionFooter();
+
+      $(window)
+        .scroll(positionFooter)
+        .resize(positionFooter);
+
+      function positionFooter() {
+        var docHeight = $(document.body).height() - $("#sticky-footer-push").height();
+        if(docHeight < $(window).height()){
+          var diff = $(window).height() - docHeight;
+          if (!$("#sticky-footer-push").length > 0) {
+            $(footer).before('<div id="sticky-footer-push"></div>');
+          }
+          $("#sticky-footer-push").height(diff);
+        }
+      }
+    }
+  });
+})(jQuery);
+
+
+
+// And, ACTION! ---
+$(document).ready(function() {
+
+  // Sticky Footer
+  $("#footerWrap").stickyFooter();
+
+});
+
+
+
+
+// Mediaelement.JS
+$('audio,video').mediaelementplayer({
+success: function(player, node) {
+  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
+}
+});
